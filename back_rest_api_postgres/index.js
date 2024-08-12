8c5d85108bebb56e9e0f4edc31edfab620dd5130
Toni H <hytontoni@gmail.com>
2024-08-12 15:22:14 +0300

    Added get by id and get all for user, stuff and loan_events database tables.

--- back_rest_api_postgres/index.js
+++ back_rest_api_postgres/index.js
@@ -3,7 +3,8 @@ const database = require('./services/database');
 
 const app = express();
 
-app.get('/', (req, res) => {
+// Gets all users from the database
+app.get('/users', (req, res) => {
     database.pool.query( 'SELECT * FROM users').then((result)=>{
         return res.status(200).json(result.rows);
     }).catch((error) => {
@@ -12,7 +13,55 @@ app.get('/', (req, res) => {
      
 })
 
+// Gets user by id from the database
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    database.pool.query( 'SELECT * FROM users WHERE id = $1', [id]).then((result)=>{
+        return res.status(200).json(result.rows);
+    }).catch((error) => {
+        return res.status(500).json({error: error.message})
+    })
+})
 
+// Gets all stuff from the database
+app.get('/stuff', (req, res) => {
+    database.pool.query( 'SELECT * FROM stuff').then((result)=>{
+        return res.status(200).json(result.rows);
+    }).catch((error) => {
+        return res.status(500).json({error: error.message})
+    })
+})
+
+// Gets stuff by id from the database
+app.get('/stuff/:id', (req, res) => {
+    const id = req.params.id;
+    database.pool.query( 'SELECT * FROM stuff WHERE id =$1', [id]).then((result)=>{
+        return res.status(200).json(result.rows);
+    }).catch((error) => {
+        return res.status(500).json({error: error.message})
+    })
+})
+
+// Gets all loans from the database
+app.get('/loans', (req, res) => {
+    database.pool.query( 'SELECT * FROM loan_events').then((result)=>{
+        return res.status(200).json(result.rows);
+    }).catch((error) => {
+        return res.status(500).json({error: error.message})
+    })
+})
+
+// Gets loan by id from the database
+app.get('/loans/:id', (req, res) => {
+    const id = req.params.id;
+    database.pool.query( 'SELECT * FROM loan_events WHERE id =$1', [id]).then((result)=>{
+        return res.status(200).json(result.rows);
+    }).catch((error) => {
+        return res.status(500).json({error: error.message})
+    })
+})
+
+// Add a new user to the database
 
 
 app.listen(3000, () => console.log('Server running on port 3000'));
--- back_rest_api_postgres/services/database.js
+++ back_rest_api_postgres/services/database.js
@@ -1,16 +1,17 @@
 const {Pool} = require('pg')
-
+//Includes the database connection information
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'stuff_loan_db',
-    password: 'salasana',
-    port: 5432,
+    // user: 'postgres',
+    // host: 'localhost',
+    // database: 'stuff_loan_db',
+    // password: 'salasana',
+    // port: 5432,
 
-    
-    //connectionString: 'postgresql://postgres:salasana@localhost:5432/postgres'
+    // This is the connection string that is used to connect to the database 
+    // Replaces the above code
+    connectionString: 'postgre://postgres:salasana@localhost:5432/stuff_loan_db'
 })
-
+// Exports the pool object so that it can be used in other files
 module.exports = {
     pool
 }
