046acfb2459db018010950b11721a577640df39c
Toni H <hytontoni@gmail.com>
2024-08-12 16:37:19 +0300

    feat: Add async/await to user, stuff, and loan_events endpoints
    Updated loans to fetch user and stuff info related to loan event

--- back_rest_api_postgres/index.js
+++ back_rest_api_postgres/index.js
@@ -4,61 +4,90 @@ const database = require('./services/database');
 const app = express();
 
 // Gets all users from the database
-app.get('/users', (req, res) => {
-    database.pool.query( 'SELECT * FROM users').then((result)=>{
+app.get('/users', async (req, res) => {
+    try{
+        const result = await database.pool.query( 'SELECT * FROM users');
         return res.status(200).json(result.rows);
-    }).catch((error) => {
+    } catch (error) {
         return res.status(500).json({error: error.message})
-    })
-     
+    }
 })
 
 // Gets user by id from the database
-app.get('/users/:id', (req, res) => {
-    const id = req.params.id;
-    database.pool.query( 'SELECT * FROM users WHERE id = $1', [id]).then((result)=>{
+app.get('/users/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const result = await database.pool.query('SELECT * FROM users WHERE id = $1', [id]);
         return res.status(200).json(result.rows);
-    }).catch((error) => {
-        return res.status(500).json({error: error.message})
-    })
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
 })
 
 // Gets all stuff from the database
-app.get('/stuff', (req, res) => {
-    database.pool.query( 'SELECT * FROM stuff').then((result)=>{
+app.get('/stuff', async (req, res) => {
+    try {
+        const result = await database.pool.query('SELECT * FROM stuff');
         return res.status(200).json(result.rows);
-    }).catch((error) => {
-        return res.status(500).json({error: error.message})
-    })
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
 })
 
 // Gets stuff by id from the database
-app.get('/stuff/:id', (req, res) => {
-    const id = req.params.id;
-    database.pool.query( 'SELECT * FROM stuff WHERE id =$1', [id]).then((result)=>{
+app.get('/stuff/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const result = await database.pool.query('SELECT * FROM stuff WHERE id = $1', [id]);
         return res.status(200).json(result.rows);
-    }).catch((error) => {
-        return res.status(500).json({error: error.message})
-    })
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
 })
 
 // Gets all loans from the database
-app.get('/loans', (req, res) => {
-    database.pool.query( 'SELECT * FROM loan_events').then((result)=>{
+app.get('/loans', async (req, res) => {
+    try {
+        const result = await database.pool.query(`
+            SELECT l.id, l.loan_start_date, l.returned_date, l.loan_end_date,
+
+            (SELECT ROW_TO_JSON(user_obj) FROM (
+                SELECT id, name, role FROM users WHERE id = l.user_id
+            ) user_obj) AS user,
+
+            (SELECT ROW_TO_JSON(stuff_obj) FROM(
+                SELECT id, name, description, quantity FROM stuff WHERE id = l.stuff_id
+            )stuff_obj) AS stuff
+
+            FROM loan_events l
+        `);
         return res.status(200).json(result.rows);
-    }).catch((error) => {
-        return res.status(500).json({error: error.message})
-    })
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
 })
 
 // Gets loan by id from the database
-app.get('/loans/:id', (req, res) => {
-    const id = req.params.id;
-    database.pool.query( 'SELECT * FROM loan_events WHERE id =$1', [id]).then((result)=>{
+app.get('/loans/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const result = await database.pool.query(`
+            SELECT l.id, l.loan_start_date, l.returned_date, l.loan_end_date,
+
+            (SELECT ROW_TO_JSON(user_obj) FROM (
+                SELECT id, name, role FROM users WHERE id = l.user_id
+            ) user_obj) AS user,
+
+            (SELECT ROW_TO_JSON(stuff_obj) FROM(
+                SELECT id, name, description, quantity FROM stuff WHERE id = l.stuff_id
+            )stuff_obj) AS stuff
+
+            FROM loan_events l WHERE id = $1`, [id]
+        );
         return res.status(200).json(result.rows);
-    }).catch((error) => {
-        return res.status(500).json({error: error.message})
-    })
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
 })
 
 // Add a new user to the database
