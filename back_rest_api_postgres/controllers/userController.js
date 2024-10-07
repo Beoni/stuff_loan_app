682192b0bbb12d5051cb87864f916e6e4e8fd9f7
Toni H <hytontoni@gmail.com>
2024-10-07 13:11:58 +0300

    refactor: Update userController.js to improve password and name update functionality

--- back_rest_api_postgres/controllers/userController.js
+++ back_rest_api_postgres/controllers/userController.js
@@ -54,24 +54,26 @@ exports.createUser = async (req, res) => {
 // Update user password
 exports.updateUserPassword = async (req,res) => {
     try {
+        const id = req.params.id;
+        const { password } = req.body;
         // Check if name and password are provided
-        if (!req.body.password) {
+        if (!password) {
             return res.status(422).json({ error: 'password is required' })
         } else { 
            // Check if password is same as the old one
             const user = await database.pool.query({
                 text: 'SELECT * FROM users WHERE id = $1',
-                values: [req.params.id]
+                values: [id]
             })
 
-            if (user.rows[0].password === req.body.password) {
+            if (user.rows[0].password === password) {
                 return res.status(409).json({ error: 'new password must be different from the old one' })
             }
         } 
         
         const result = await database.pool.query({
             text: 'UPDATE users SET passwor = $1 WHERE id = $2 RETURNING *',
-            values: [req.body.password, req.params.id]
+            values: [password, id]
         })   
 
         if (result.rowCount === 0) {
@@ -88,34 +90,36 @@ exports.updateUserPassword = async (req,res) => {
 // Update user name
 exports.updateUserName = async (req,res) => {
     try {
-        // Check if name is provided
-        if (!req.body.name) {
+        const id = req.params.id;
+        const { name } = req.body;
+         // Check if name is provided
+        if (!name) {
             return res.status(422).json({ error: 'name is required' })
-        } else if (req.body.name) { 
+        } else if (name) { 
            // Check if name is same as the old one
             const user = await database.pool.query({
                 text: 'SELECT * FROM users WHERE name = $1',
-                values: [req.params.name]
+                values: [name]
             })
 
-            if (user.rows[0].name === req.body.name) {
+            if (user.rows[0].name === name) {
                 return res.status(409).json({ error: 'new name must be different from the old one' })
               } 
         } else {
             // Check if the new name already exists
             const existisUser = await database.pool.query({
                 text: 'SELECT EXISTS (SELECT * FROM users WHERE name = $1)',
-                values: [req.body.name]
+                values: [name]
             })
 
             if (existisUser.rows[0].exists) {
-                return res.status(409).json({ error: `user name ${req.body.name} already exists` });
+                return res.status(409).json({ error: `user name ${name} already exists` });
             }
         }
 
         const result = await database.pool.query({
             text: 'UPDATE users SET name = $1 WHERE id = $2 RETURNING *',
-            values: [req.body.name, req.params.id]
+            values: [name, id]
         })
         
         // Check if id exists
@@ -130,12 +134,39 @@ exports.updateUserName = async (req,res) => {
     }
 }
 
+// Update user role
+exports.updateUserRole = async (req,res) => {
+    try {
+        const id = req.params.id;
+        const role = req.body.role;
+        // Check if role is provided
+        if (!role) {
+            return res.status(422).json({ error: 'role is required' })
+        } 
+        
+        const result = await database.pool.query({
+            text: 'UPDATE users SET role = $1 WHERE id = $2 RETURNING *',
+            values: [role, id]
+        })   
+
+        if (result.rowCount === 0) {
+            return res.status(404).json({ error: 'user not found' })
+        }
+
+        return res.status(200).json(result.rows[0]);
+        
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
 // Delete user
 exports.deleteUser = async (req,res) => {
     try {
+        const id = req.params.id;
         const result = await database.pool.query({
             text: 'DELETE FROM users WHERE id = $1 RETURNING *',
-            values: [req.params.id]
+            values: [id]
         })
         if (result.rowCount === 0) {
             return res.status(404).json({ error: 'user not found' })
